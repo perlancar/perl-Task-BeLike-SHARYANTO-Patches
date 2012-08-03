7a68f5c02d3d23afcef09c208503081328900067
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-08-03 15:02:41 +0700

    Initial commit

---
+++ lib/Task/BeLike/SHARYANTO/Patches.pm
@@ -0,0 +1,22 @@
+package Task::BeLike::SHARYANTO::Patches;
+
+# VERSION
+
+=head1 DESCRIPTION
+
+This task will install all SHARYANTO's patch modules.
+
+=pkgroup Included modules
+
+=pkg HTTP::Daemon::patch::ipv6
+
+=pkg LWP::UserAgent::patch::https_hard_timeout
+
+=pkg LWP::UserAgent::patch::log_response
+
+=pkg Net::HTTP::Methods::patch::log_request
+
+=cut
+
+1;
+# ABSTRACT: SHARYANTO's patch modules
