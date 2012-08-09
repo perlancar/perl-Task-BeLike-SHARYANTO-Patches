24816d80b58bd9cbcde754a1882235e5d1deb100
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-08-09 13:48:18 +0700

    Rename modules

--- lib/Task/BeLike/SHARYANTO/Patches.pm
+++ lib/Task/BeLike/SHARYANTO/Patches.pm
@@ -8,13 +8,13 @@ This task will install all SHARYANTO's patch modules.
 
 =pkgroup Included modules
 
-=pkg HTTP::Daemon::patch::ipv6
+=pkg HTTP::Daemon::Patch::IPv6
 
-=pkg LWP::UserAgent::patch::https_hard_timeout
+=pkg LWP::UserAgent::Patch::HTTPSHardTimeout
 
-=pkg LWP::UserAgent::patch::log_response
+=pkg LWP::UserAgent::Patch::LogResponse
 
-=pkg Net::HTTP::Methods::patch::log_request
+=pkg Net::HTTP::Methods::Patch::LogRequest
 
 =cut
 
